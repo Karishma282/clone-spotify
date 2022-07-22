0761f9c5ca55f9431ec39b8bcc910f0ce95fd1f5
Karishma Nikam <78778719+Karishma282@users.noreply.github.com>
2022-07-22 13:17:23 +0530

    Update reducer.js

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,21 +1,41 @@
-
-
 import { reducerCases } from "./Constants";
 
+export const initialState = {
+    token : null,
+    playlists: [],
+    userInfo: null,
+    selectedPlaylistId: "37i9dQZF1DWSwxyU5zGZYe",
+    selectedPlaylist: null
+};
 
-export const initialState={
-    token:null,
-}
-
-const reducer=(state,action )=>{
-    switch(action.type){
+const reducer = (state,action) => {
+    switch (action.type){
         case reducerCases.SET_TOKEN:{
             return{
-                ...state,token:action.token,
+                ...state,token: action.token,
+            };
+        }
+        case reducerCases.SET_PLAYLISTS : {
+            return {
+                ...state,
+                playlists: action.playlists,
+            };
+        }
+        case reducerCases.SET_USER: {
+            return {
+                ...state,
+                userInfo: action.userInfo,
+            };
+        }
+        case reducerCases.SET_PLAYLIST: {
+            return{
+                ...state,
+                selectedPlaylist: action.selectedPlaylist
             }
         }
         default:
-            return state
+            return state;
     }
 };
+
 export default reducer;
